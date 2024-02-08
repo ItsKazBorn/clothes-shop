b1fc556fc8c032d48c48ddc50a75631d4183d0a9
Kaz Born <itskazborn@gmail.com>
2024-02-08 02:16:32 -0300

    Character now is able to equip items

--- Clothing Shop/Assets/Assets/Scripts/Character/CharacterAnimationController.cs
+++ Clothing Shop/Assets/Assets/Scripts/Character/CharacterAnimationController.cs
@@ -1,8 +1,61 @@
+using System.Collections.Generic;
 using UnityEngine;
+using Zenject;
 
 public class CharacterAnimationController : MonoBehaviour
 {
+    [Inject] private SignalBus m_signalBus;
+    [Inject] private SpriteSheetManager m_spriteSheetManager;
+
+    [SerializeField] private bool IsPlayer;
     [SerializeField] private CharacterSpriteLayerController[] m_spriteLayerControllers;
+
+    private Dictionary<ItemSlot, GameItem> m_equippedItems;
+    
+    private const string m_charCode = "a";
+    private const int m_charPage = 1;
+    
+    private void Awake()
+    {
+        m_equippedItems = new Dictionary<ItemSlot, GameItem>();
+        
+        if (IsPlayer) m_signalBus.Subscribe<OnGameItemEquipedSignal>(EquipItem);
+    }
+
+    private void OnDestroy()
+    {
+        m_signalBus.TryUnsubscribe<OnGameItemEquipedSignal>(EquipItem);
+    }
+
+    private void EquipItem(OnGameItemEquipedSignal args)
+    {
+        EquipItem(args.Item);
+    }
+
+    public void EquipItem(GameItem item)
+    {
+        if (m_equippedItems.ContainsKey(item.Slot)) m_equippedItems[item.Slot] = item;
+        else m_equippedItems.Add(item.Slot, item);
+        
+        UpdateSpriteSheets();
+    }
+
+    private void UpdateSpriteSheets()
+    {
+        foreach (CharacterSpriteLayerController layer in m_spriteLayerControllers)
+        {
+            if (m_equippedItems.ContainsKey(layer.Slot))
+            {
+                layer.SetNewSpriteSheet(m_spriteSheetManager.GetSpriteSheetName(m_charCode, m_charPage, m_equippedItems[layer.Slot]));
+                layer.SetLayerEnabled(true);
+            }
+            else
+            {
+                if (layer.Slot == ItemSlot.BASE) return;
+                layer.SetLayerEnabled(false);
+            }
+        }
+    }
     
     public void SetMovementParameters(Vector2 movement, float speed)
     {
