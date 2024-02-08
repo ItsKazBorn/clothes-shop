e250ecf1e870fa373d9bdbd1202a9833dc1d21d2
Kaz Born <itskazborn@gmail.com>
2024-02-08 04:53:59 -0300

    ReuploadBuild

--- Build/Clothing Shop_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/lumpedcpp/Lump_libil2cpp_codegen.cpp
+++
@@ -1,4 +0,0 @@
-#include "il2cpp-config.h"
-#include "E:\Programs\Unity\2021.3.2f1\Editor\Data\il2cpp\libil2cpp\codegen\il2cpp-codegen-common.cpp"
-#include "E:\Programs\Unity\2021.3.2f1\Editor\Data\il2cpp\libil2cpp\codegen\il2cpp-codegen-il2cpp.cpp"
-#include "E:\Programs\Unity\2021.3.2f1\Editor\Data\il2cpp\libil2cpp\codegen\il2cpp-codegen-tiny.cpp"
--- Build/Clothing Shop_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/lumpedcpp/Lump_libil2cpp_debugger.cpp
+++
@@ -1,2 +0,0 @@
-#include "il2cpp-config.h"
-#include "E:\Programs\Unity\2021.3.2f1\Editor\Data\il2cpp\libil2cpp\debugger\il2cpp-api-debugger.cpp"
