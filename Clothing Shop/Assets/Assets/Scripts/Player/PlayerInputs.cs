130cb3285277320d78f8b3dce6c6032468dda4ac
Kaz Born <itskazborn@gmail.com>
2024-02-08 03:31:38 -0300

    Remove useless code

--- Clothing Shop/Assets/Assets/Scripts/Player/PlayerInputs.cs
+++ Clothing Shop/Assets/Assets/Scripts/Player/PlayerInputs.cs
@@ -9,22 +9,6 @@ public class PlayerInputs : MonoBehaviour
     
     [SerializeField] private CharacterMovement m_characterMovement;
     [SerializeField] private Interactor m_interactor;
-    [SerializeField] private PlayerInput m_playerInput;
-
-    private const string m_uiMap = "UI";
-    private const string m_playerMap = "Player";
-
-    private void Start()
-    {
-        m_signalBus.Subscribe<OnClothingShopOpenedSignal>(OnShopOpened);
-        m_signalBus.Subscribe<OnClothingShopClosedSignal>(OnShopClosed);
-    }
-
-    private void OnDestroy()
-    {
-        m_signalBus.Unsubscribe<OnClothingShopOpenedSignal>(OnShopOpened);
-        m_signalBus.Unsubscribe<OnClothingShopClosedSignal>(OnShopClosed);
-    }
 
     private void OnRun(InputValue value)
     {
@@ -42,18 +26,4 @@ public class PlayerInputs : MonoBehaviour
         m_interactor.Interact();
     }
 
-    private void OnShopOpened(OnClothingShopOpenedSignal args)
-    {
-        SetMapInputsEnabled(false);
-    }
-    
-    private void OnShopClosed()
-    {
-        SetMapInputsEnabled(true);
-    }
-
-    private void SetMapInputsEnabled(bool enabled)
-    {
-        m_playerInput.SwitchCurrentActionMap(enabled ? m_playerMap : m_uiMap);
-    }
 }
