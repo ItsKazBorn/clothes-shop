a612364790016435ed1230eb83176453e90d9806
Kaz Born <itskazborn@gmail.com>
2024-02-07 22:34:43 -0300

    Included UI Input System

--- Clothing Shop/Assets/Assets/Scripts/Player/PlayerInputs.cs
+++ Clothing Shop/Assets/Assets/Scripts/Player/PlayerInputs.cs
@@ -1,11 +1,29 @@
-
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Zenject;
 
 public class PlayerInputs : MonoBehaviour
 {
+    [Inject] private SignalBus m_signalBus;
+    
     [SerializeField] private CharacterMovement m_characterMovement;
     [SerializeField] private Interactor m_interactor;
+    [SerializeField] private PlayerInput m_playerInput;
+
+    private const string m_uiMap = "UI";
+    private const string m_playerMap = "Player";
+
+    private void Start()
+    {
+        m_signalBus.Subscribe<OnClothingShopOpenedSignal>(OnShopOpened);
+        m_signalBus.Subscribe<OnClothingShopClosedSignal>(OnShopClosed);
+    }
+
+    private void OnDestroy()
+    {
+        throw new NotImplementedException();
+    }
 
     private void OnRun(InputValue value)
     {
@@ -21,5 +39,20 @@ public class PlayerInputs : MonoBehaviour
     private void OnInteract(InputValue value)
     {
         m_interactor.Interact();
+    }
+
+    private void OnShopOpened(OnClothingShopOpenedSignal args)
+    {
+        SetMapInputsEnabled(false);
+    }
+    
+    private void OnShopClosed()
+    {
+        SetMapInputsEnabled(true);
+    }
+
+    private void SetMapInputsEnabled(bool enabled)
+    {
+        m_playerInput.SwitchCurrentActionMap(enabled ? m_playerMap : m_uiMap);
     }
 }
